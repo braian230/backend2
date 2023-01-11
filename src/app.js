3784245a78dfd63761991f76e09e744fff6ccafa
braian230 <arielmoreno19@hotmail.com>
2023-01-11 17:57:34 -0300

    Entrega jueves 12

--- src/app.js
+++ src/app.js
@@ -1,13 +1,30 @@
-import express from 'express'
-import cartsRoutes from './routes/cartsRoutes.js';
-import productsRoutes from './routes/productsRoutes.js';
+const express = require('express')
+const handlebars = require('express-handlebars')
+const apiRoutes = require('./routers/app.routers')
+const viewsRoutes = require('./routers/views/views.router')
+const path = require('path')
+const { Server } = require ('socket.io')
 
+const PORT = 8080
+const app = express()
 
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.listen(8080, () => console.log("Servidor listo puerto 8080"))
-app.use('/api/products/', productsRoutes)
-app.use('/api/carts/', cartsRoutes)
-app.use(express.static('public'))
+app.use(express.urlencoded({ extended:true }))
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+app.use('/statics', express.static(path.resolve(__dirname, '../public')))
+app.use('/api', apiRoutes)
+app.use('/', viewsRoutes)
+
+
+const httpServer = app.listen(PORT, ()=>{
+    console.log('Listening on port => ', PORT)
+})
+
+const socketServer = new Server(httpServer) 
+
+socketServer.on('connection', socket => {
+    console.log('new client connected')
+    app.set('socket', socket)
+})
--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 import multer from 'multer'
-
+const path = require('path')
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public/img')
+        cb(null, path.resolve(__dirname, '..public/img'))
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
